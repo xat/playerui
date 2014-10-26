f9d1ec08eae710ca88bf2f0a29cca3e7633462e0
Simon Kusterer <simon@soped.com>
2014-10-26 20:16:55 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# playerui
+
+minimalistic mediaplayer commandline ui.
+
+### sample
+
+```javascript
+var ui = require('playerui');
+
+// add some labels
+ui.setLabel('source', 'Source', 'Ty Segall - Manipulator (Album)');
+ui.setLabel('state', 'Playing', 'Playing...');
+ui.setLabel('position', 'Position', '10:00 / 50:00');
+
+// specify which labels to show
+ui.showLabels('source', 'state', 'position');
+
+// set progress bar to 20%
+ui.setProgress(20);
+
+// output
+ui.render();
+```
+The above code will render this into the commandline
+
+![Example UI](/example.png?raw=true "Example UI")
+
+### Installation
+
+`npm install playerui`
+
+## License
+Copyright (c) 2014 Simon Kusterer
+Licensed under the MIT license.
---
+++ index.js
@@ -0,0 +1,90 @@
+var chalk = require('chalk');
+var log = require('single-line-log').stdout;
+var extend = require('xtend');
+var pad = require('pad');
+
+module.exports = function(opts) {
+  var options = extend({
+    defaultStyle: chalk.grey.inverse,
+    progressStyle: chalk.cyan.inverse,
+    paddingLeft: 2,
+    paddingTop: 1,
+    paddingBottom: 1,
+    labelPadding: 5
+  }, opts || {});
+
+  var visibleLabels = [];
+  var labels = {};
+  var progress = 0;
+
+  // fetch all labels currently displayed
+  var getActiveLabels = function() {
+    return visibleLabels.map(function(l) {
+      return labels[l];
+    });
+  };
+
+  // check which label has the most characters
+  var longestLabel = function() {
+    return getActiveLabels()
+      .reduce(function(memo, l) {
+        if (!l || !l.label || l.label.length <= memo) return memo;
+        return l.label.length;
+      }, 0);
+  };
+
+  return {
+
+    showLabels: function() {
+      visibleLabels = Array.prototype.slice.call(arguments);
+      return this;
+    },
+
+    setLabel: function(name, label, value) {
+      labels[name] = {
+        label: label,
+        value: value
+      }
+      return this;
+    },
+
+    setProgress: function(val) {
+      progress = val;
+      return this;
+    },
+
+    render: function() {
+      var cols = process.stdout.columns;
+      var labelPadding = longestLabel() + options.labelPadding;
+      var pos = cols * (progress / 100);
+      var emptyLine = pad('', cols);
+
+      rows = getActiveLabels()
+        .map(function(l) {
+          return pad(pad('', options.paddingLeft)
+                      + pad(l.label, labelPadding)
+                      + ': ' + l.value, cols);
+        });
+
+      var addPaddingTop = function(padding) {
+        while (padding--) rows.unshift(emptyLine);
+      };
+
+      var addPaddingBottom = function(padding) {
+        while (padding--) rows.push(emptyLine);
+      };
+
+      addPaddingTop(options.paddingTop);
+      addPaddingBottom(options.paddingBottom);
+
+      rows = rows.map(function(line) {
+        return options.progressStyle(line.substr(0, pos)) +
+                options.defaultStyle(line.substr(pos));
+      });
+
+      log(rows.join("\n") + "\n");
+      return this;
+    }
+
+  };
+};
